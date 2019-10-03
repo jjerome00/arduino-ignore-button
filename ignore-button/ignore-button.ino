6004e3d8a189c3e5cc4dc4ca0482ec253242cbdf
Jason Jerome <jason.jerome@mabonservices.com>
2019-10-03 09:01:42 -0700

    Switched LED indicator from the built-in to the LED on the (external) button itself.

--- ignore-button/ignore-button.ino
+++ ignore-button/ignore-button.ino
@@ -1,13 +1,14 @@
 //www.elegoo.com
 //2016.12.08
 /*
- * I believe this is the one that works!
+ * This is the one that works!
  * 
  */
 
 
 int gDoorPin = 5;
 int buttonPin = 9;
+int buttonLEDPin = 10;
 
 String msg = "seconds to wait: ";
 int secondsToWait = 20;
@@ -17,10 +18,18 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  
   pinMode(gDoorPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonLEDPin, OUTPUT);
   
+  init();
+}
+
+void init() 
+{
   digitalWrite(gDoorPin, HIGH);
+  setButtonLED_ON();
   Serial.println("Hi - ready");
 }
 
@@ -29,7 +38,7 @@ void loop()
   if (digitalRead(buttonPin) == LOW)
   {
     Serial.println("button pressed");
-    digitalWrite(LED_BUILTIN, HIGH);
+    setButtonLED_OFF();
 
     startOpener();
 
@@ -37,9 +46,18 @@ void loop()
     delay(secondsToWait * 1000);
 
     Serial.println("done - ready for more");
-    digitalWrite(LED_BUILTIN, LOW);
+    setButtonLED_ON();
   }
 }
+
+void setButtonLED_ON() {
+  digitalWrite(buttonLEDPin, HIGH);
+}
+
+void setButtonLED_OFF() {
+  digitalWrite(buttonLEDPin, LOW);
+}
+
 
 void startOpener() {
   Serial.println("gDoor - activating");
